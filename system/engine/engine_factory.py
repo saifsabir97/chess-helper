45ece69333983040f6e29896dca3d5fe64376254
saifsabir97 <saifsabir97@gmail.com>
2022-02-05 13:48:49 +0530

    fix parameter type in EngineFactory

--- system/engine/engine_factory.py
+++ system/engine/engine_factory.py
@@ -6,7 +6,7 @@ from system.engine.stockfish_engine import StockfishEngine
 class EngineFactory:
 
     @staticmethod
-    def get_engine(engine_type: EngineType = EngineType) -> Engine:
+    def get_engine(engine_type: EngineType) -> Engine:
         if engine_type == EngineType.stockfish:
             return StockfishEngine()
         else:
